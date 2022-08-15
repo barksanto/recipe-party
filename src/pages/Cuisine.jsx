1a61a56b91abfda062dd032b776b0e4ad727aad0
Barkley <barkley.santo@gmail.com>
2022-08-15 18:55:52 +0800

    rendering unique cuisine page with fetch from params when specific link is clicked

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -6,25 +6,58 @@ import {useState, useEffect} from "react"
 
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
-  let params = useParams()
+  let params = useParams() // @ use params from URL
 
   const getCuisine = async (name) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOON_API_KEY}&cuisine=${name}`)
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOON_API_KEY}&cuisine=${name}`
+    );
     const recipes = await data.json();
-    setCuisine(recipes.results)
-    console.log(cuisine.type)
+    setCuisine(recipes.results) // @ set state of cuisine to results of fetch
+    // console.log(recipes)
   }
 
   useEffect(() => {
-    console.log(params.type)
-    // getCuisine()
-  }, [params])
+    console.log(params)
+    getCuisine(params.type) //@ pass params (name) to getCuisine
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params.type]);
 
   return (
-    <div>
-
-    </div>
+    <Grid>
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+          <img src={item.image} alt="" />
+          <h4>{ item.title}</h4>
+        </Card>
+      )
+      })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fit,minmax(20rem, 1fr));
+grid-gap: 3rem;
+`
+
+const Card = styled.div`
+img{
+  width: 100%;
+  border-radius: 2rem;
+}
+
+a{
+  text-decoration: none;
+}
+
+h4{
+  text-align: center;
+  padding: 1rem;
+}
+  
+`
 
 export default Cuisine
