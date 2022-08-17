104485f7723c40dcfc4710cc723029df0cfdd544
Barkley <barkley.santo@gmail.com>
2022-08-17 11:31:31 +0800

    add active class to Instructions and Ingredients buttons - dangerouslySetInnrHtml for recipe summary

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import {useParams} from "react-router-dom"
 
 function Recipe() {
   let params = useParams()
   const [details, setDetails] = useState({})
+  const [activeTab, setActiveTab] = useState('instructions')
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -16,8 +17,61 @@ function Recipe() {
   }, [params.name])
 
   return (
-    <div>{details.title}</div>
+    <DetailWrapper>
+      
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt="" />
+      </div>
+
+      <Info>
+        <Button className={activeTab === "instructions" ? "active" : ""} onClick={() => setActiveTab("instructions")}>Instructions</Button>
+        <Button className={activeTab === "ingredients" ? "active" : ""} onClick={() => setActiveTab("ingredients")}>Ingredients</Button>
+        <div>
+          <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+        </div>
+      </Info>
+      
+    </DetailWrapper>
   )
 }
 
+const DetailWrapper = styled.div`
+margin-top: 10rem;
+margin-bottom: 5rem;
+display: flex;
+
+.active{
+  background: linear-gradient(35deg, #494949, #313131);
+  color: white;
+}
+
+h2{
+  margin-bottom: 2rem;
+}
+
+li{
+  font-size: 1.2rem;
+  line-height: 2.5rem;
+}
+
+ul{
+  margin-top: 2rem;
+}
+`
+const Button = styled.button`
+padding: 1rem 2rem;
+color: #313131;
+background: white;
+border: 2px solid black;
+margin-right: 2rem;
+font-weight: 600;
+`
+
+const Info = styled.div`
+/* display: flex; */
+/* height: 50px; */
+/* flex-direction: row; */
+/* margin-left: 1rem; */
+`
 export default Recipe
